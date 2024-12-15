c2cfe2155571f8c9f8dd2b9c4eafa92e0165245e
Anthony <tonynarciso2003@gmail.com>
2024-12-15 09:57:11 -0500

    creando la class hoja de calculo 2

--- hojaCalculo.h
+++ hojaCalculo.h
@@ -17,7 +17,7 @@ class HojaCalculo
     //covirtiendo uan referencia de una celda a indices de fila y columna
     bool ConvertirCelda( const string& ref, int& fila, int& columna) const;
     //convertir una expresion de una celda infija a posfija para evaluarla 
-    auto InfijaAposFija(const string& expresion) const;
+    string InfijaAposFija(const string& expresion) const;
     //evaluando la expresion posfija
     auto EvaluandoPosfija(const string& postfija) const;
     //validand rangos de celda
@@ -26,10 +26,16 @@ class HojaCalculo
     auto EvaluarFuncion(const string& funcion, const string& range) const;
 
     public:
-    HojaCalculo(int fila, int columna);
+    HojaCalculo(int fila, int columna); //aqui esta el constructor que inicializa la hoja de calculo
+    //asignando contenido a una celda
+    void EstablecerContenidoCelda(int fila, int colummna, const string& valor);
+    //para obtener el contenido textual de la celda
+    string ObtenerContenidoCelda(int fila, int columna) const;
+    //obteniendo el valor numerico elavuado de una celda
+    auto ObtenerValorCelda(int fila, int columna) const;
+    //mostrando la hoja de calcula en la pantalla
+    void mostrar() const;
 
-
-
-}
+};
 
 #endif //HOJACALCULO_H
